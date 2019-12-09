97d30471fce0eb8636c28ce95e1ce829d756a358
Andre Setiawan A <ahmadiandre@gmail.com>
2019-12-09 08:02:26 +0700

    Initial Commit

---
+++ Eja-Angka/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Eja-Angka
+//
+//  Created by Andre Setiawan A on 09/12/19.
+//  Copyright © 2019 Andre Setiawan A. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
